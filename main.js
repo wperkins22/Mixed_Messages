df19aa09a87253ab5961d142206532c063677dad
Wyatt Perkins <perkinswyatt@gmail.com>
2021-06-10 12:14:36 -0600

    store message components

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log('Hello, World!')
+const openers = [
+  "Did you know,",
+  "Fun fact:",
+  "Here's something they don't teach you in school:",
+  "It is a fact that",
+  "A little known fact is that",
+];
+
+const facts = [
+  "chickens have large talons",
+  "you can learn to fly",
+  "mattresses are made of nails",
+  "styrofoam is not recyclable",
+  "french fries are good for your soul",
+];
+
+const conditions = ['at nighttime', 'but only when it\'s warm out', 'as long as you paid for it', 'if someone near you is wearing a sweater', 'after the accident']
+
+
