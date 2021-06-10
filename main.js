778cbc9b8c212f9d458e304abd8757a6dde8bc54
Wyatt Perkins <perkinswyatt@gmail.com>
2021-06-10 12:49:33 -0600

    add functionality to recieve and print user's name

--- main.js
+++ main.js
@@ -41,4 +41,15 @@ function createMessage() {
 
 const printMessage = () => console.log(createMessage())
 
-printMessage()
+const readline = require('readline').createInterface({
+	input: process.stdin,
+	output: process.stdout,
+})
+
+readline.question(`What's your name?\n`, (name) => {
+	console.log(`Hi ${name}! Here's a random 'fact'!`)
+	printMessage()
+	readline.close()
+})
+
+// printMessage()
