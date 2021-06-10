85f917574a845ea8ac87aedcfd9c2e209f4e4eea
Wyatt Perkins <perkinswyatt@gmail.com>
2021-06-10 12:34:21 -0600

    finish createMessage, printMessage functions, update readme

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # Mixed Messages
 ## Wyatt Perkins
 
-### Program prints a random message every time it is run
+### Program prints a random "fact" every time it is run
+
+**To run the program:**
+1. download Mixed_Messages
+2. navigate to the Mixed_Messages directory
+3. run *main.js* from the command line
--- main.js
+++ main.js
@@ -1,19 +1,44 @@
 const openers = [
-  "Did you know,",
-  "Fun fact:",
-  "Here's something they don't teach you in school:",
-  "It is a fact that",
-  "A little known fact is that",
-];
+	'Did you know,',
+	'Fun fact:',
+	"Here's something they don't teach you in school:",
+	'It is a fact that',
+	'A little known fact is that',
+	"I shouldn't tell you this, but",
+	'A secret I know is that',
+]
 
 const facts = [
-  "chickens have large talons",
-  "you can learn to fly",
-  "mattresses are made of nails",
-  "styrofoam is not recyclable",
-  "french fries are good for your soul",
-];
+	'chickens have large talons',
+	'you can learn to fly',
+	'a mattress is made of nails',
+	'styrofoam is not recyclable',
+	'french fries are good for your soul',
+	'sleeping is good for your health',
+	'people who run are faster than people who walk',
+]
 
-const conditions = ['at nighttime', 'but only when it\'s warm out', 'as long as you paid for it', 'if someone near you is wearing a sweater', 'after the accident']
+const conditions = [
+	'at nighttime',
+	"but only when it's warm out",
+	'as long as you paid for it',
+	'if someone near you is wearing a sweater',
+	'after the accident',
+	'in January',
+	'if the left foot is bigger than the right',
+]
 
+function createMessage() {
+	const openerIndex = Math.floor(Math.random() * 7)
+	const factIndex = Math.floor(Math.random() * 7)
+	const conditionIndex = Math.floor(Math.random() * 7)
+	let punctuation = '.'
+	if (openerIndex === 0) {
+		punctuation = '?'
+	}
+	return `${openers[openerIndex]} ${facts[factIndex]} ${conditions[conditionIndex]}${punctuation}`
+}
 
+const printMessage = () => console.log(createMessage())
+
+printMessage()
